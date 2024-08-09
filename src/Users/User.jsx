a4ef53e4ec3f7716b7f7fdce39efc4fb9dd189ad
ali <3alimirhashemi@gmail.com>
2024-08-09 20:28:32 +0330

    axios-get-data-session36

--- src/Users/User.jsx
+++ src/Users/User.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import swal from "sweetalert";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const User = ()=>{
+    const [ users,setUsers ] = useState([]);
+    useEffect(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{
+          setUsers(res.data)
+          
+      }).catch(err=>{
+          console.log(err);
+      })
+
+    }, []);
+
     const handelDel = (timeIde)=>{
         swal({
             title: `از حذف رکورد ${timeIde} اطمینان دارید؟`,
@@ -24,38 +37,46 @@ const User = ()=>{
     }
     return(
         <div id="content" className="p-4 p-md-5 pt-5">
-            <div className="mb-4">
             <h2 className="mb-4">کاربران</h2>
+            {users.length ? (
+                <div className="mb-4">
                 <table className="table table-striped">
                     <thead>
                         <tr>
                         <th scope="col">شماره</th>
-                        <th scope="col">نام</th>
-                        <th scope="col">نام خانوادگی</th>
+                        <th scope="col"> نام و نام خانوادگی</th>
+                        <th scope="col">نام کاربری</th>
                         <th scope="col">موبایل</th>
                         <th scope="col">ایمیل</th>
                         <th scope="col">آدرس</th>
                         <th scope="col">عملیات</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>محسن</td>
-                        <td>احمدی</td>
-                        <td>09121234567</td>
-                        <td>mohsen@gmail.com</td>
-                        <td>ایران-تهران-ازادی-نصیری</td>
-                        <td className="h5 mx-2">
-                            <Link to="/user/add/2">
-                            <i className="fa fa-edit text-warning"></i>
-                            </Link>
-                            <i className="fa fa-trash text-danger mx-2" onClick={()=>{handelDel(1)}}></i>
-                        </td>
-                        </tr>
-                    </tbody>
+                    {users.map(u=>(
+                                    <tbody>
+                                          <tr>
+                                            <th scope="row">{u.id}</th>
+                                            <td>{u.name}</td>
+                                            <td>{u.username}</td>
+                                            <td>{u.phone}</td>
+                                            <td>{u.email}</td>
+                                            <td>{u.address.city},{u.address.street},{u.address.suit},{u.address.zipcode}</td>
+                                            <td className="h5 mx-2">
+                                                <Link to="/user/add/2">
+                                                <i className="fa fa-edit text-warning"></i>
+                                                </Link>
+                                                <i className="fa fa-trash text-danger mx-2" onClick={()=>{handelDel(1)}}></i>
+                                            </td>
+                                          </tr>
+                                      </tbody>
+                    ))}
                 </table>
             </div>
+            ):(
+              <div className="mb-4">
+                  <h3 className="display-4 text-danger">لطفا منتظر بمانید.</h3>
+              </div>
+            )}
             <hr />
             <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
             <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
