8e29fc332caeeb3f0dd40c49ff9ecce02fcf363c
ali <3alimirhashemi@gmail.com>
2024-08-15 18:26:42 +0330

    create-full-page-session41

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -5,7 +5,7 @@ const Post = ()=>{
     const [data , setData] = useState([]);
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/posts').then(res=>{
-            console.log(res.data);
+
             setData(res.data)
         }).catch(err=>{
             console.log(err);
@@ -15,31 +15,42 @@ const Post = ()=>{
 
     return(
         <div id="content" className="p-4 p-md-5 pt-5">
-            <h2 className="mb-4">پست</h2>        
-            {data.map(u=>(
+            <h2 className="mb-4">پست</h2> 
+            {data.length ? (
+                            
                 <div className="container">
-                <h1 className="text-center badge badge-info">{u.title}</h1>
-                <p className="justify-content-center">{u.body}</p>
-                <div className="d-flex justify-content-end">
-                    <div className="m-1">
-                        <span className="fa fa-user-circle-o"></span>
-                        <p className="badge badge-light pl-2">{u.title}</p>
-                    </div>
-                    <div className="m-1">
-                        <span className="fa fa-comment"></span>
-                        <p className="badge badge-light pl-2">{u.id}</p>
-                    </div>
-                    <div className="m-1">
-                        <span className="fa fa-heart"></span>
-                        <p className="badge badge-light pl-2">{u.userId}</p>
-                    </div>
-                    
+                    {data.map(u=>(
+                        <div>
+                        <h1 className="text-center badge badge-info">{u.title}</h1>
+                        <p className="justify-content-center">{u.body}</p>
+                        <div className="d-flex justify-content-end">
+                            <div className="m-1">
+                                <span className="fa fa-user-circle-o"></span>
+                                <p className="badge badge-light pl-2">{u.title}</p>
+                            </div>
+                            <div className="m-1">
+                                <span className="fa fa-comment"></span>
+                                <p className="badge badge-light pl-2">{u.id}</p>
+                            </div>
+                            <div className="m-1">
+                                <span className="fa fa-heart"></span>
+                                <p className="badge badge-light pl-2">{u.userId}</p>
+                            </div>
+                                    
+                        </div>
+                        <hr/>
+                        </div>
+                    ))}
                 </div>
-                <hr/>
-                </div>
-            ))}
+            ):(
+                <>
+                <h1 className="display-4 text-info">لطفا منتظر بمانید...</h1>
+                </>
+            )}       
+
             
         </div>
+
     )
 }
 export default Post;
