2326f3f4c8f9335d05912b498accf0401097757d
ali <3alimirhashemi@gmail.com>
2024-08-25 16:00:59 +0330

    session-47

--- src/Posts/Post.jsx
+++ src/Posts/Post.jsx
@@ -2,21 +2,47 @@
 import { useEffect, useState } from "react";
 import { getServicePost } from "../services/ServicePost";
 import { Link, useNavigate } from "react-router-dom";
+import SweetAlert from "../HOC/SweetAlert";
 
 
 const Post = ()=>{
     const [post , setPost] = useState([]);
+    const [mainPost , setMainPost] = useState([]);
+    const [uId, setUid] = useState("");
+
     const navigate = useNavigate();
-    const handelSearch = () => {}
-    const handelDel = (postId) => {}
+
+    const handelSearch = () => {
+        if (uId > 0)setPost(mainPost.filter(p=>p.userId === uId))
+        else setPost(mainPost)
+        
+    }
+
+    const handelDel = async (postId) => {
+
+    }
+    
     const getPost = async ()=>{
         const res = await getServicePost();
         setPost(res.data)
+        setMainPost(res.data)
+
 
     }
     useEffect(()=>{
+        console.log('first log');
         getPost();
-    },[])
+    } , [])
+
+    useEffect(()=>{
+        console.log('every change');
+    })
+
+    useEffect(()=>{
+        console.log('every uid change');
+        handelSearch()
+
+    } , [uId])
 
 
     return(
@@ -25,7 +51,7 @@ const Post = ()=>{
             <div className="mb-5 px-4">
                 <form className="form-inline my-2 my-lg-0">
                     <div className="form-group d-flex">
-                        <input type="search" className="form-control mr-sm-2 border" onChange={handelSearch} placeholder="جستجو براساس شماره ..." />
+                        <input type="search" className="form-control mr-sm-2 border" value={uId} onChange={(e)=>setUid(e.target.value)} placeholder="جستجو براساس شماره ..." />
                         <button className="form-control border btn btn-dark m-1" type="submit"><span className="fa fa-search mr-0 p-2" ></span></button>
                     </div>
                     <Link to="/post/add">
@@ -39,14 +65,14 @@ const Post = ()=>{
             {post.length ? (
                             
                 <div className="container">
-                    {post.slice(0.40).map(u=>(
+                    {post.map(u=>(
                         <div>
                         <h1 className="text-center badge badge-info">{u.title}</h1>
                         <p className="justify-content-center">{u.body}</p>
                         <div className="d-flex justify-content-end">
                             <div className="m-1">
                                 <span className="fa fa-user-circle-o text-info"></span>
-                                <p className="badge badge-light pl-2">{u.userId}</p>
+                                <p className="badge badge-light pl-2 pointer" onClick={()=>setUid(u.userId)}>{u.userId}</p>
                             </div>
                             <div className="m-1">
                                 <span className="fa fa-comment text-success"></span>
@@ -57,8 +83,8 @@ const Post = ()=>{
                                 <p className="badge badge-light pl-2">{u.userId}</p>
                             </div>
                             <div className="m-1">
-                                <i className="fa fa-edit text-warning pointer" onClick={()=>{navigate(`post/add`)}}></i>
-                                <i className="fa fa-trash text-danger mx-2 pointer" onClick={()=>{handelDel()}}></i>
+                                <i className="fa fa-edit text-warning pointer" onClick={()=>{navigate(`post/add/${u.id}`)}}></i>
+                                <i className="fa fa-trash text-danger mx-2 pointer" onClick={()=>{handelDel(u.id)}}></i>
                             </div>
                                     
                         </div>
@@ -78,4 +104,4 @@ const Post = ()=>{
 
     )
 }
-export default Post;
+export default SweetAlert(Post);
